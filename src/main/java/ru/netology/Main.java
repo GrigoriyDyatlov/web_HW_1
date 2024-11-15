85ccd464108edbc9355d4c01f44c7fa5bca778a1
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-11-15 16:46:38 +0300

    added Server class.

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,22 @@
 package ru.netology;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExecutorService threadPoll = Executors.newFixedThreadPool(64);
+        try (final var serverSocket = new ServerSocket(9999)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                threadPoll.execute(new Server(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
